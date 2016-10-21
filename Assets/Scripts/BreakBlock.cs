1461e631c1b5e64e0a570ba396ca1bcb79b2af80
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-10-21 14:02:14 +0900

    ReStart

---
+++ Assets/Scripts/BreakBlock.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakBlock : MonoBehaviour
+{
+	
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			Vector2 tapPoint = UnityEngine.Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Collider2D collider = Physics2D.OverlapPoint (tapPoint);
+
+			if (collider.gameObject.tag == "Block")
+			{
+				GameObject gameobject = collider.transform.gameObject;
+				gameobject.SetActive (false);
+
+				Debug.Log ("broken block");
+			}
+		}
+	}
+
+}
