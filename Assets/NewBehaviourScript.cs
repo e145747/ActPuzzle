f97dace774347ab942f059d13ee9bef51006c1f1
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-10-21 17:14:52 +0900

    Change player sprite and Add c#-file

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	private Animator anime;
+
+	void Start ()
+	{
+		anime = GetComponent<Animator>();
+	}
+
+	void Update ()
+	{
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Block")
+		{
+			if (FlagManager.Instance.flags [1] == true)
+			{
+				anime.SetBool ("Attached", false);
+				FlagManager.Instance.flags [1] = false;
+			}
+
+			else
+			{
+				anime.SetBool ("Attached", true);
+				FlagManager.Instance.flags [1] = true;
+			}
+		}
+	}
+}
