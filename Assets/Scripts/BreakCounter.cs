5b72896c7a5d548b11a35989c40125344689a952
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-11-01 16:04:10 +0900

    Display block's score

--- Assets/Scripts/BreakCounter.cs
+++ Assets/Scripts/BreakCounter.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BreakCounter : MonoBehaviour
 {
-	public int BreakCount;
+	public Text scoreText;
+	public int  breakcount;
 
 	void Start ()
 	{
-		BreakCount = 0;
+		breakcount = 0;
+		scoreText.text = "Break Block : 0";
 	}
 
 	void Update ()
 	{
+		scoreText.text = "Break Block : " + breakcount.ToString();
 	}
 
 	public void Count ()
 	{
-		BreakCount = BreakCount + 1;
+		breakcount = breakcount + 1;
 	}
 }
