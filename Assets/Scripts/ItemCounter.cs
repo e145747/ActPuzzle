328e289dbb83399505cb2a2e948c1e4e3b7cc7cc
e145747 <e145747@ie.u-ryukyu.ac.jp>
2016-11-10 18:11:47 +0900

    Add Members products and Make Item "?????" , and Minor Changes

---
+++ Assets/Scripts/ItemCounter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ItemCounter : MonoBehaviour
+{
+	public Text scoreText2;
+	public int  itemcount;
+
+	void Start ()
+	{
+		itemcount = 0;
+		scoreText2.text = "Get \"?????\" : 0/3";
+	}
+
+	void Update ()
+	{
+		if (itemcount == 1)
+		{
+			scoreText2.text = "Get \"?????\" : 1/3";
+		}
+
+		if (itemcount == 2)
+		{
+			scoreText2.text = "Get \"?????\" : 2/3";
+		}
+
+		if (itemcount == 3)
+		{
+			scoreText2.text = "Get \"?????\" : 3/3";
+		}
+	}
+
+	public void Count ()
+	{
+		itemcount = itemcount + 1;
+	}
+}
---
+++ Assets/Scripts/ItemTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTrigger : MonoBehaviour
+{
+	GameObject itemcount;
+
+	void Start ()
+	{
+		itemcount = GameObject.Find("MainCamera");
+	}
+
+	void Update ()
+	{
+	}
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+			ItemCounter count = itemcount.GetComponent<ItemCounter>();
+			count.Count();
+
+			gameObject.SetActive (false);
+		}
+	}
+}
